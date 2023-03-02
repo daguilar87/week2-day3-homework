a246657cd98729b0fa14aaac3f0e5c9c29cd1ec4
Daniel Aguilar <daguilar8749@gmail.com>
2023-03-02 14:36:34 -0800

    first commit

---
+++ circumference_module.py
@@ -0,0 +1,8 @@
+# 2) Has a function to calculate the circumference of a circle
+import math
+
+def circle_circum(diameter):
+    return diameter * math.pi
+
+
+print(circle_circum(5))
---
+++ sqft_module.py
@@ -0,0 +1,7 @@
+# 1) Has a function to calculate the square footage of a house
+# Reminder of Formula: Length X Width == Area
+
+def square_foot(length, width):
+    return (length * width)
+
+print(square_foot(25,5))
